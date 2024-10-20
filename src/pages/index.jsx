0f83e42edda3a303c59a4c992767e544ce24b31e
Le Van Quy <vannn.quy@gmail.com>
2024-10-20 11:39:05 +0700

    up

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,10 +3,6 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main>
-      <h2>Đây là trang chủ</h2>
-      <Link href="/woman-20-10">
-        News
-      </Link>
     </main>
   );
 }
--- src/pages/news.jsx
+++ src/pages/news.jsx
@@ -1,7 +1,6 @@
 function News() {
     return (
         <main>
-            <h2 className='text-red-500'>Đây là trang tin tức</h2>
         </main>
     );
 }
